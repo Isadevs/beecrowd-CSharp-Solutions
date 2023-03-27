c8ead91d060dd04f03c49313147641091aba560d
Isadevs <contato.isabellaramos@hotmail.com>
2023-03-27 06:23:45 -0300

    atualização exercícios

---
+++ 1098/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Você deve fazer um programa que apresente a sequencia conforme o exemplo abaixo.
+
+Entrada
+Não há nenhuma entrada neste problema.
+
+Saída
+Imprima a sequencia conforme exemplo abaixo.
+
+Exemplo de Entrada	Exemplo de Saída
+I=0 J=1
+I=0 J=2
+I=0 J=3
+I=0.2 J=1.2
+I=0.2 J=2.2
+I=0.2 J=3.2
+.....
+I=2 J=?
+I=2 J=?
+I=2 J=?*/
